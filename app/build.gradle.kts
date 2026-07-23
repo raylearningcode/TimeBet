545b0fd76e903b20ba3cd89418526f9425a861a9
raylearningcode <raybelajarr@gmail.com>
2026-07-23 19:59:48 +0800

    chore: bump to v1.0.11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.timebet.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
